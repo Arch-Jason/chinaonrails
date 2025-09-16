d8fefab75fcfbcc2a41b8db0d2c1b00748d31850
Arch-Jason <jasonking393939@gmail.com>
2025-09-16 16:06:21 +0800

    announcement added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Desc from './components/desc';
 import { lines } from './lines';
 import { desc } from './desc';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal } from 'react-bootstrap';
 
 function App() {
   const availableYears = Object.keys(lines).map(Number);
@@ -16,12 +17,27 @@ function App() {
   const [showSharePoints, setShowSharePoints] = useState<boolean>(true);
   const [showLines, setShowLines] = useState<boolean>(true);
 
+  const [showAnnouncement, setShowAnnouncement] = useState(true);
   return (
     <div className="App">
-        <Topbar setShowSharePoints={setShowSharePoints} setShowLines={setShowLines} />
-        <Timeline year={year} setYear={setYear} showLines={showLines} />
-        <Map lines={currentLines} showSharePoints={showSharePoints} showLines={showLines} />
-        <Desc desc={currentDesc} showLines={showLines} />
+      <Modal
+        size="lg"
+        centered
+        show={showAnnouncement}
+        onHide={() => setShowAnnouncement(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>公告</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>请您做一名有素质的火车迷，不乱上传点位。谢谢。</p>
+          <a href="mailto:report@chinaonrails.org">点我发送反馈邮件</a>
+        </Modal.Body>
+      </Modal>
+      <Topbar setShowSharePoints={setShowSharePoints} setShowLines={setShowLines} />
+      <Timeline year={year} setYear={setYear} showLines={showLines} />
+      <Map lines={currentLines} showSharePoints={showSharePoints} showLines={showLines} />
+      <Desc desc={currentDesc} showLines={showLines} />
     </div>
   );
 }
